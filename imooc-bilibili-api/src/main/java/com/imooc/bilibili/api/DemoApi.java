1f717094c3e4a64f0c7bf8a7bbeb4d0bd799c7e0
2483428575 <2483428575@qq.com>
2022-09-01 20:43:24 +0800

    1.5 数据库查询数据demo

---
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/DemoApi.java
@@ -0,0 +1,19 @@
+package com.imooc.bilibili.api;
+
+import com.imooc.bilibili.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApi {
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/query")
+    public Long query(Long id){
+        return demoService.query(id);
+    }
+
+}
