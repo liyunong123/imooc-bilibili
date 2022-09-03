952395f179cd26da6d97f2a4c2e959515585f735
2483428575 <2483428575@qq.com>
2022-09-04 00:39:01 +0800

    3.6  全局异常处理

---
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/handler/CommonGlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.imooc.bilibili.service.handler;
+
+import com.imooc.bilibili.domain.JsonResponse;
+import com.imooc.bilibili.domain.exception.ConditionException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)  //表明这个异常处理器优先级是最高的
+public class CommonGlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class) //标识这是一个异常的处理器，value标识这是什么类型的处理器-》exception设计全局的
+    @ResponseBody
+    public JsonResponse<String> commonExceptionHandler(HttpServletRequest request, Exception e){
+        String errorMsg = e.getMessage();
+        if(e instanceof ConditionException){
+            String errorCode = ((ConditionException)e).getCode();
+            return new JsonResponse<>(errorCode, errorMsg);
+        }else{
+            return new JsonResponse<>("500",errorMsg);
+        }
+    }
+}
